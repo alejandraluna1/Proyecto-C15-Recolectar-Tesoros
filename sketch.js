b2ff0c6cd7e934f0acdf8c3213d93e4dc046812a
alejandraluna1 <81326858+alejandraluna1@users.noreply.github.com>
2021-05-24 08:42:18 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,7 +3,7 @@ var pathImg,boyImg,cashImg,diamondsImg,jwelleryImg,swordImg;
 var treasureCollection = 0;
 var cashG,diamondsG,jwelleryG,swordGroup;
 
-//Game States
+//Estados del Juego
 var PLAY=1;
 var END=0;
 var gameState=1;
@@ -21,13 +21,13 @@ function preload(){
 function setup(){
   
   createCanvas(400,600);
-// Moving background
+// Fondo en movimiento
 path=createSprite(200,200);
 path.addImage(pathImg);
 path.velocityY = 4;
 
 
-//creating boy running
+//crear al niño corriendo
 boy = createSprite(70,580,20,20);
 boy.addAnimation("SahilRunning",boyImg);
 boy.scale=0.08;
@@ -49,7 +49,7 @@ function draw() {
   edges= createEdgeSprites();
   boy.collide(edges);
   
-  //code to reset the background
+  //código para reiniciar el fondo
   if(path.y > 400 ){
     path.y = height/2;
   }
@@ -96,7 +96,7 @@ function draw() {
   drawSprites();
   textSize(20);
   fill(255);
-  text("Treasure: "+ treasureCollection,150,30);
+  text("Tesoro: "+ treasureCollection,150,30);
   }
 
 }
